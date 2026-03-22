a73e1d8be0752223f465694a12db293e63b14c82
Beatriz <beatriz.gonzalezv@um.es>
2026-03-22 22:07:10 +0100

    Initial commit

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const PORT = process.env.PORT || 8080;
 
 export const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'localhost',    //hecho
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'Pituca1900*',
-    database: process.env.DB_NAME || 'bearbitrage',
+    database: process.env.DB_NAME || 'bearbitrage',   //hecho
     timezone: 'Z',
     port: process.env.DB_PORT || 3306
 };
